b7741cdf097894f971f30169b826f1a653f1aec4
levent-ozgn <levent.ozgn@eduge.ch>
2026-03-14 20:29:01 +0100

    refactor: fixed module problem

--- src/js/Product.js
+++ src/js/Product.js
@@ -1,61 +1,61 @@
-// export class Product {
-//   #name;
-//   #energy;
-//   #fat;
-//   #saturatedFat;
-//   #carbohydrates;
-//   #sugars;
-//   #fiber;
-//   #proteins;
-//   #salt;
-//   #sodium;
-//   #quantity;
+export default class Product {
+  #name;
+  #energy;
+  #fat;
+  #saturatedFat;
+  #carbohydrates;
+  #sugars;
+  #fiber;
+  #proteins;
+  #salt;
+  #sodium;
+  #quantity;
 
-//   constructor(data) {
-//     this.#name = data.product_name_en || "";
-//     this.#energy = data.nutriments?.energy || "?";
-//     this.#fat = data.nutriments?.fat || "?";
-//     this.#saturatedFat = data.nutriments?.saturated_fat || "?";
-//     this.#carbohydrates = data.nutriments?.carbohydrates || "?";
-//     this.#sugars = data.nutriments?.sugars || "?";
-//     this.#fiber = data.nutriments?.fiber || "?";
-//     this.#proteins = data.nutriments?.proteins || "?";
-//     this.#salt = data.nutriments?.salt || "?";
-//     this.#sodium = data.nutriments?.sodium || "?";
-//     this.#quantity = data.quantity || "";
-//   }
+  constructor(data) {
+    this.#name = data.product_name_en || "";
+    this.#energy = data.nutriments?.energy || "?";
+    this.#fat = data.nutriments?.fat || "?";
+    this.#saturatedFat = data.nutriments?.saturated_fat || "?";
+    this.#carbohydrates = data.nutriments?.carbohydrates || "?";
+    this.#sugars = data.nutriments?.sugars || "?";
+    this.#fiber = data.nutriments?.fiber || "?";
+    this.#proteins = data.nutriments?.proteins || "?";
+    this.#salt = data.nutriments?.salt || "?";
+    this.#sodium = data.nutriments?.sodium || "?";
+    this.#quantity = data.quantity || "";
+  }
 
-//   get name() {
-//     return this.#name;
-//   }
-//   get energy() {
-//     return this.#energy;
-//   }
-//   get fat() {
-//     return this.#fat;
-//   }
-//   get saturatedFat() {
-//     return this.#saturatedFat;
-//   }
-//   get carbohydrates() {
-//     return this.#carbohydrates;
-//   }
-//   get sugars() {
-//     return this.#sugars;
-//   }
-//   get fiber() {
-//     return this.#fiber;
-//   }
-//   get proteins() {
-//     return this.#proteins;
-//   }
-//   get salt() {
-//     return this.#salt;
-//   }
-//   get sodium() {
-//     return this.#sodium;
-//   }
-//   get quantity() {
-//     return this.#quantity;
-//   }
-// }
+  get name() {
+    return this.#name;
+  }
+  get energy() {
+    return this.#energy;
+  }
+  get fat() {
+    return this.#fat;
+  }
+  get saturatedFat() {
+    return this.#saturatedFat;
+  }
+  get carbohydrates() {
+    return this.#carbohydrates;
+  }
+  get sugars() {
+    return this.#sugars;
+  }
+  get fiber() {
+    return this.#fiber;
+  }
+  get proteins() {
+    return this.#proteins;
+  }
+  get salt() {
+    return this.#salt;
+  }
+  get sodium() {
+    return this.#sodium;
+  }
+  get quantity() {
+    return this.#quantity;
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import { Product } from "./Product.js";
+import Product from './Product.js';
 
 const constants = {
   OK: 200,
@@ -94,81 +94,5 @@ async function neededData(productCode) {
   } catch (error) {
     console.error("Error fetching product:", error);
     return null;
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////
-
-
-
-class Product {
-  #name;
-  #energy;
-  #fat;
-  #saturatedFat;
-  #carbohydrates;
-  #sugars;
-  #fiber;
-  #proteins;
-  #salt;
-  #sodium;
-  #quantity;
-
-  constructor(data) {
-    this.#name = data.product_name_en || "";
-    this.#energy = data.nutriments?.energy || "?";
-    this.#fat = data.nutriments?.fat || "?";
-    this.#saturatedFat = data.nutriments?.saturated_fat || "?";
-    this.#carbohydrates = data.nutriments?.carbohydrates || "?";
-    this.#sugars = data.nutriments?.sugars || "?";
-    this.#fiber = data.nutriments?.fiber || "?";
-    this.#proteins = data.nutriments?.proteins || "?";
-    this.#salt = data.nutriments?.salt || "?";
-    this.#sodium = data.nutriments?.sodium || "?";
-    this.#quantity = data.quantity || "";
-  }
-
-  get name() {
-    return this.#name;
-  }
-  get energy() {
-    return this.#energy;
-  }
-  get fat() {
-    return this.#fat;
-  }
-  get saturatedFat() {
-    return this.#saturatedFat;
-  }
-  get carbohydrates() {
-    return this.#carbohydrates;
-  }
-  get sugars() {
-    return this.#sugars;
-  }
-  get fiber() {
-    return this.#fiber;
-  }
-  get proteins() {
-    return this.#proteins;
-  }
-  get salt() {
-    return this.#salt;
-  }
-  get sodium() {
-    return this.#sodium;
-  }
-  get quantity() {
-    return this.#quantity;
   }
 }
